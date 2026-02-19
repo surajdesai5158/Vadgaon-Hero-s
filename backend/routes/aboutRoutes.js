e39e107becebbc7ade510694f0598152c86b3d70
surajdesai5158 <surajdesai5158@gmail.com>
2026-02-19 20:56:17 +0530

    first commit

--- backend/routes/aboutRoutes.js
+++ backend/routes/aboutRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
-import upload from "../middleware/upload.js";
+// import upload from "../middleware/upload.js";
 import { addAbout, getAbout, deleteAbout } from "../controllers/aboutController.js";
 
 const aboutrouter = express.Router();
 
-aboutrouter.post("/", upload.array("photos", 10), addAbout);
+// TEMP FIX: remove upload middleware
+aboutrouter.post("/", addAbout);
 aboutrouter.get("/", getAbout);
 aboutrouter.delete("/:id", deleteAbout);
 
